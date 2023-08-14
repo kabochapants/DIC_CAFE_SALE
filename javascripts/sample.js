752ac2b85878d7226c3b79dba1b07fff73166524
chisa <chisa.i@icloud.com>
2023-08-14 15:06:49 +0900

    first commit

--- javascripts/sample.js
+++ javascripts/sample.js
@@ -1,8 +1,21 @@
-const priceElement = document.getElementById("product");
-const numberElement = document.getElementById("number");
+function addLink() {
+  const element = document.createElement("a");
+  element.setAttribute("href", "https://diveintocode.jp/");
+  element.innerText = "DPro";
+  const parentElement = document.getElementsByTagName("div")[0];
+  parentElement.appendChild(element);
+};
 
-function calc() {
-  const price = parseInt(priceElement.value);
-  const number = parseInt(numberElement.value);
-  window.alert(`${price}円が${number}個。小計は${price * number}円です`);
-}
+window.onload = addLink;
+
+$(document).ready(function() {
+  $("#btn").click(function() {
+    alert("クリックされました")
+  })
+});
+
+$(function(){
+  $("#btn").on("click", function() {
+    $("p").show("500", "linear")
+  });
+});
